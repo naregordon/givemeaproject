518e556b01abdbc9af55a7e0cd77daad8a129961
Nabuma <Nabuma@github.com>
2014-08-13 14:53:28 +0200

    maj photo

--- apps/header.php
+++ apps/header.php
@@ -1,5 +1,11 @@
 <?php
 
+if(isset($_SESSION['username'])){
+	$photo_user = '';
+	$res = mysqli_query($db,"SELECT photo FROM user WHERE username = '".$_SESSION['username']."'");
+	$photo_user = mysqli_fetch_assoc($res);
+}
+
 if(isset($_POST['logmail'], $_POST['logpass'], $_POST['logsubmit'])) {
 	$email = $_POST['logmail'];
 	$password = $_POST['logpass'];
@@ -13,16 +19,10 @@ if(isset($_POST['logmail'], $_POST['logpass'], $_POST['logsubmit'])) {
 	}
 	elseif($data['email'] === $email && $data['password'] === $password && $data['type'] === 'dev') {
 		$_SESSION['username'] = $data['username'];
-		$photo_user = '';
-		$res = mysqli_query($db,"SELECT photo FROM user WHERE username = '".$_SESSION['username']."'");
-		$photo_user = mysqli_fetch_assoc($res);
 		$page="dev-profil";
 	}
 	elseif($data['email'] === $email && $data['password'] === $password && $data['type'] === 'pp') {
 		$_SESSION['username'] = $data['username'];
-		$photo_user = '';
-		$res = mysqli_query($db,"SELECT photo FROM user WHERE username = '".$_SESSION['username']."'");
-		$photo_user = mysqli_fetch_assoc($res);
 		$page="pp-profil";
 	}
 }
