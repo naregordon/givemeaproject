78fbb6abac162111001e5272b45f5f6bb7e8bc02
mma <mmabad@live.fr>
2014-08-13 13:55:14 +0200

    dev search plus aff open search plus aff

--- apps/dev-recherche.php
+++ apps/dev-recherche.php
@@ -1,3 +1,28 @@
 <?php
+require("models/projet.class.php");
+function getProjetsSelection($db)
+{
+	$tab=array();
+	$res = mysqli_query($db, "SELECT * FROM project ORDER BY projetdate DESC");
+	while ($data = mysqli_fetch_object($res, 'Projet'))
+		$tab[] = $data;
+	return $tab;
+}
+
+function getProjetSearch($db)
+{
+	$tabSearch=array();
+	$res = mysqli_query($db, "SELECT * FROM project WHERE projetnom LIKE '%".$_POST['search']."%' ORDER BY projetdate DESC");
+    while ($data = mysqli_fetch_object($res,"Projet")) 
+        $tabSearch[] = $data;
+    return $tabSearch;
+}
+
+if(isset($_POST['search']) && isset($_POST['searchBtn'])) {    
+	$listeProjetSearch=getProjetSearch($db);
+}
+else
+$listeProjetSelection=getProjetsSelection($db);
+
 require("views/dev-recherche.phtml");
 ?>
--- apps/liste-projets.php
+++ apps/liste-projets.php
@@ -1,7 +1,29 @@
 <?php
-$res = mysqli_query($db, "SELECT * FROM project ORDER BY id DESC");
-$listeProjets = mysqli_fetch_assoc($db, $res);
+require("models/projet.class.php");
 
+function getProjetsSelection($db)
+{
+	$tab=array();
+	$res = mysqli_query($db, "SELECT * FROM project ORDER BY projetdate DESC");
+	while ($data = mysqli_fetch_object($res, 'Projet'))
+		$tab[] = $data;
+	return $tab;
+}
 
-require('views/liste-projets.phtml')
+function getProjetSearch($db)
+{
+	$tabSearch=array();
+	$res = mysqli_query($db, "SELECT * FROM project WHERE projetnom LIKE '%".$_POST['search']."%' ORDER BY projetdate DESC");
+    while ($data = mysqli_fetch_object($res,"Projet")) 
+        $tabSearch[] = $data;
+    return $tabSearch;
+}
+
+if(isset($_POST['search']) && isset($_POST['searchBtn'])) {    
+	$listeProjetSearch=getProjetSearch($db);
+}
+else
+$listeProjetSelection=getProjetsSelection($db);
+
+require('views/liste-projets.phtml');
 ?>
