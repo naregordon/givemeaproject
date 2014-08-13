078d437404551a3bf906233845832a33797168b7
mma <mmabad@live.fr>
2014-08-13 10:09:51 +0200

    upload photo

--- apps/pp-post.php
+++ apps/pp-post.php
@@ -1,6 +1,13 @@
 <?php
 
 if(isset($_POST['projetpost'], $_POST['projetnom'], $_POST['projetdescription'], $_POST['profilrecherche'], $_POST['projetwords'])) {
+	require ('models/uploadFile.class.php');
+		$upload = new upLoadFile();
+
+		$tmp_name=$_FILES['photo_project']['tmp_name'];
+		$name=$_FILES['photo_project']['name'];
+		$upload->uploadProject($tmp_name,$name);
+		
 	$projetnom = $_POST['projetnom'];
 	$projetdescription = $_POST['projetdescription'];
 	$projetwords = $_POST['projetwords'];
@@ -8,7 +15,8 @@ if(isset($_POST['projetpost'], $_POST['projetnom'], $_POST['projetdescription'],
 	$tempstravail = $_POST['tempstravail'];
 	$username = $_SESSION['username'];
 
-	mysqli_query($db, "INSERT INTO `givemeaproject`.`project` (`projetnom`, `projetdescription`, `projetwords`, `profilrecherche`, `projetdate`, `tempstravail`, `username` , `statut`) VALUES ('".$projetnom."', '".$projetdescription."','".$projetwords."','".$profilrecherche."',NOW(),'".$tempstravail."','".$username."','selection')");
+
+	mysqli_query($db, "INSERT INTO `givemeaproject`.`project` (`projetnom`, `projetdescription`, `projetwords`, `profilrecherche`, `projetdate`, `tempstravail`, `username` , `statut`, `photo`) VALUES ('".$projetnom."', '".$projetdescription."','".$projetwords."','".$profilrecherche."',NOW(),'".$tempstravail."','".$username."','selection','".$name."')");
 
 	require("apps/pp-profil.php");
 }
--- models/uploadFileProject.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class upLoadFileProject
-{
-	function uploadProject($tmp_name,$name)
-	{
-		move_uploaded_file($tmp_name,'sources/images/photo_project/'.$name);
-	}		
-}
-?>
