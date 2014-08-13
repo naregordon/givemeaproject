062189d334d0da0b41940fd49ffcfa670c631e72
momsdela <elaouni.m@gmail.com>
2014-08-13 16:50:35 +0200

    Mohammed update

--- apps/dev-projets.php
+++ apps/dev-projets.php
@@ -1,3 +1,21 @@
 <?php
+require("models/projet.class.php");
 
-require("views/dev-projets.phtml");
+function getProjetsSelectiondev($db)
+{
+	$tab=array();
+	$res = mysqli_query($db, "SELECT * FROM project WHERE statut='selection' ORDER BY projetdate DESC");
+	while ($data = mysqli_fetch_object($res,'Projet'))
+		$tab[] = $data;
+	return $tab;
+}
+
+$listeProjetSelectiondev=getProjetsSelectiondev($db);
+
+if (isset($_POST['idp'])){
+	$idp=$_POST['idp'];
+	$tab=array();
+	$res = mysqli_query($db, "SELECT * FROM project WHERE id='".$idp."'");
+	$data = mysqli_fetch_object($res, 'Projet');
+}
+require("views/dev-recherche.phtml");
