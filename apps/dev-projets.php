a6a290d3e95326aa0d4248830cbce8532744c4d5
Lucas Haladjian <lucashaladjian@pc1.home>
2014-08-14 16:54:22 +0200

    postuler a un projet

--- apps/dev-projets.php
+++ apps/dev-projets.php
@@ -12,10 +12,17 @@ require("models/projet.class.php");
 
 // $listeProjetSelectiondev=getProjetsSelectiondev($db);
 
-if (isset($_POST['idp'])){
+if (isset($_POST['idp'], $_POST['voirFicheProjet'])){
 	$idp=$_POST['idp'];
 	$tab=array();
 	$res = mysqli_query($db, "SELECT * FROM project WHERE id='".$idp."'");
 	$data = mysqli_fetch_object($res, 'Projet');
+	require("views/dev-fiche-projet.phtml");
 }
-require("views/dev-fiche-projet.phtml");
+
+if (isset($_POST['postulerProjet'])) {
+	$selection = $_SESSION['username'];
+	$idp=$_POST['idp'];
+	mysqli_query($db, "INSERT INTO candidats (id_projet, username_candidat) VALUES ('".$idp."','".$selection."') ");
+	require("views/dev-recherche.phtml");
+}
