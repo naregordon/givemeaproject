d1ae0c8ce95ebc25496747cdf5dae1c8d34923b8
momsdela <elaouni.m@gmail.com>
2014-08-19 15:17:17 +0200

    update liste postulant

--- apps/pp-postulants.php
+++ apps/pp-postulants.php
@@ -1,5 +1,5 @@
 <?php
-
+$mess='';
 if (isset($_POST['selectSubmit'],$_POST['id'])) 
 {
 	if (isset($_POST['selectPostulant'])) 
@@ -7,7 +7,10 @@ if (isset($_POST['selectSubmit'],$_POST['id']))
 		$idpostulant=$_POST['selectPostulant'];
 		$id=$_POST['id'];
 		$res =mysqli_query($db,"UPDATE project SET selection='".$idpostulant."', statut='en cours' WHERE id=".$id."");
+		$mess='Vous avez selectionné <strong>'.$idpostulant.' </strong> pour votre projet';
+		require("views/confirmation.phtml");
 	}
 }
-
+else{
 require("views/pp-postulants.phtml");
+}
