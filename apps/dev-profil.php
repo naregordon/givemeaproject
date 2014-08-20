aaec4edeb5b857307b8e97c7a9f9d68ed90a6e03
gig75 <you@example.com>
2014-08-20 17:07:22 +0200

    affichage billets  dashboard dev

--- apps/dev-profil.php
+++ apps/dev-profil.php
@@ -1,3 +1,22 @@
 <?php
+
+require("models/projet.class.php");
+
+$username=$_SESSION['username'];
+
+
+function getProjets($db,$query)
+{	
+	$tab=array();
+	$res = mysqli_query($db, $query);
+	while ($data = mysqli_fetch_object($res, 'Projet'))
+		$tab[] = $data;
+	return $tab;
+}
+
+$listeProjetEncours = getProjets($db,"SELECT * FROM project WHERE statut='en cours' AND selection='".$username."' ORDER BY projetdate DESC");
+
+$listeProjetSelection = getProjets($db,"SELECT * FROM project LEFT JOIN candidats ON project.id = candidats.id_projet WHERE project.statut='selection' AND candidats.username_candidat='".$username."' ORDER BY projetdate DESC");
+
 require("views/dev-profil.phtml");
 ?>
--- apps/dev-projets.php
+++ apps/dev-projets.php
@@ -14,7 +14,6 @@ require("models/projet.class.php");
 
 if (isset($_POST['idp'], $_POST['voirFicheProjet'])){
 	$idp=$_POST['idp'];
-	$tab=array();
 	$res = mysqli_query($db, "SELECT * FROM project WHERE id='".$idp."'");
 	$data = mysqli_fetch_object($res, 'Projet');
 	require("views/dev-fiche-projet.phtml");
